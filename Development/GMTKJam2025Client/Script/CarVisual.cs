c0c0151d5dcf6b7998c7c6ff3b81b7bed5639fdd
KaynSD <keith.a.evans.1@gmail.com>
2025-08-02 11:34:58 +0200

    Adding Car Visuals

---
+++ Development/GMTKJam2025Client/Script/CarVisual.cs
@@ -0,0 +1,12 @@
+using Godot;
+using System;
+
+public partial class CarVisual: Node3D
+{
+	private Node3D SteeringL;
+	private Node3D SteeringR;
+	public override void _Ready()
+	{
+		base._Ready();
+	}
+}
